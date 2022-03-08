11b61def28ad096bfec148e1182f2400f37855e1
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-03-08 10:16:20 +0000

    add components and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import Articles from './components/Articles';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path = "/" element={<Home />} />
+          <Route path = "/articles" element={<Articles />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
