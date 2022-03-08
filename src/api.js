747574308615b6c790e12354c5550d7c1898e450
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-03-08 10:15:41 +0000

    create fetchAllArticles function

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const fetchAllArticles = () => {
+  const url = "https://jc-news.herokuapp.com/api/articles";
+    return axios.get(`${url}`).then((res) => {
+      return res.data
+    });
+};
