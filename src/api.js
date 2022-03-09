fbae4b236ce0d8be84ba0355afad0c5f854dec3e
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-03-09 10:32:31 +0000

    fetchAllTopics and fetchArticlesByTopic functions

--- src/api.js
+++ src/api.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
+
 export const fetchAllArticles = () => {
   const url = "https://jc-news.herokuapp.com/api/articles";
+    return axios.get(`${url}`).then((res) => {
+      return res.data
+    });
+};
+
+export const fetchAllTopics = () => {
+  const url = "https://jc-news.herokuapp.com/api/topics";
+    return axios.get(`${url}`).then((res) => {
+      return res.data.topics
+    });
+};
+
+export const fetchArticlesByTopic = (topic) => {
+  const url = `https://jc-news.herokuapp.com/api/articles?topic=${topic}`;
     return axios.get(`${url}`).then((res) => {
       return res.data
     });
