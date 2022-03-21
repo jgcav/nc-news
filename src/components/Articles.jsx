55c5673df84c31cda2331b920aaa983add38b1d5
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-03-21 23:18:41 +0000

    add order and sort-by filters

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,30 +1,59 @@
 import {React, useState, useEffect} from 'react'
 import ArticleCard from './ArticleCard'
 import { fetchAllArticles } from '../api'
+import { useSearchParams } from 'react-router-dom'
+import Topics from './Topics'
+import OrderFilter from './Order'
+import SortByFilter from './SortBy'
 
 export default function Articles() {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const topic = searchParams.get('topic')
+
     const [articles, setArticles] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    const [selectOrder, setSelectOrder] = useState()
+    const [selectSortBy, setSelectSortBy] = useState()
+
+    let query = '?'
+    let queryArr = []
+
+    if (selectOrder) {
+        queryArr.push(`order=${selectOrder}`)
+    }
+
+    if (selectSortBy) {
+        queryArr.push(`sort_by=${selectSortBy}`)
+    }
+
+    query += queryArr.join('&')
 
     useEffect(() => {
-        setIsLoading(true)
-        fetchAllArticles()
-        .then((res) => {
-            setIsLoading(false)
+        fetchAllArticles(query).then((res) => {
             setArticles(res)
+            setIsLoading(false)
         })
-    }, [])
+    }, [query])
 
+
+
+    if (isLoading) return <p>Loading...</p>
+    
     return (
         <div className="container">
             <h3 className="subHeader">Articles</h3>
-            {isLoading ? (<p>Loading...</p>) : (
+            <div className="filters">
+                <SortByFilter setSelectSortBy={setSelectSortBy}/>
+                <br />
+                <OrderFilter setSelectOrder={setSelectOrder}/>
+            </div>
                 <div className="articles">
                     {articles.map((article) => {
                     return <ArticleCard key = {article.article_id} article={article}/>;
         })}
          </div>
-            )}
         </div>
     )
 }
