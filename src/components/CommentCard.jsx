cba59b7064dda49f1b2c61c1d9c204d6bd980adc
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-04-06 18:41:54 +0100

    add onClick function to delete comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { fetchArticleComments, deleteComment } from '../api'
 
 export default function CommentCard(comment) {
+
+
+    const handleClick = () => {
+        deleteComment(comment.comment_id).then(() => {fetchArticleComments(comment.article_id).then((comments) => {
+            comment.setComments(comments)
+        })})
+    }
+
     return (
             <div className="row">
         <div className="column">
@@ -10,7 +19,11 @@ export default function CommentCard(comment) {
             <dt>{comment.body}</dt>
             <br />
             <dt className="commentAuthor">{comment.author}</dt>
+            <br />
+            <dt>Date: {comment.created_at.substr(0,10)}</dt>
             </dl>
+            <br />
+            <button className="deleteComment" onClick={handleClick}>Delete</button>
         </div>
         </div>
         </div>
